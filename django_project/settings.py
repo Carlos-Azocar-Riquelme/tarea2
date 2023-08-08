15410770796ec8b014b43bb751535782b12734ed
Carlos-Azocar-Riquelme <78894939+Carlos-Azocar-Riquelme@users.noreply.github.com>
2023-08-08 22:23:49 +0000

    Commit archivos de configuración

--- django_project/settings.py
+++ django_project/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'carlosazocar_tareas',
+        'USER': 'carlosazocar_usertarea',
+        'PASSWORD':'miseryn73',
+        'HOST': 'postgresql-carlosazocar.alwaysdata.net'
     }
 }
 
